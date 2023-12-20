7b98f306e375d2c548c03abd7f8e19f3785356ea
Lucia Fresno <lucia.fresno.olmeda@gmail.com>
2023-12-20 13:59:32 +0100

    Seccion 19 clase 266 terminada

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CountriesRoutingModule } from './countries-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    SelectorPageComponent
+  ],
+  imports: [
+    CommonModule,
+    CountriesRoutingModule,
+    ReactiveFormsModule
+  ]
+})
+export class CountriesModule { }
